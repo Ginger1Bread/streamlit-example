4ed2d51e485e0d2f96c8560ebe0e9b31ec8a5d9a
Ginger1Bread <127928939+Ginger1Bread@users.noreply.github.com>
2023-03-15 11:22:13 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+print("Hello, world!")  # Outputs "Hello, world!" to the console
+
+x = 10
+y = 20
+print("The sum of", x, "and", y, "is", x + y)  # Outputs "The sum of 10 and 20 is 30" to the console
+
+name = "John"
+age = 25
+print("My name is", name, "and I am", age, "years old.", sep="-")  # Outputs "My name is-John-and I am-25-years old." to the console with hyphens as separators
+
+print("Hello, world!", end="")  # Outputs "Hello, world!" to the console without a newline character at the end
